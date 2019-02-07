f6f3910fc238409f9265c930c31148764a31bc6c
shwetha-2627 <shwethanm.2627@gmail.com>
2019-02-07 12:18:05 +0530

    fi

--- TYSS/src/localDate/LocalDate1.java
+++ TYSS/src/localDate/LocalDate1.java
@@ -8,9 +8,6 @@ public static void main(String [] args) {
 	System.out.println("current day:"+LocalDate.now().getDayOfMonth());
 	System.out.println("current month:"+LocalDate.now().getMonth());
 	System.out.println("current year:"+LocalDate.now().getYear());
-	//int eDay = LocalDate.now().getDayOfMonth();
-	//System.out.println(eDay);
-	//demo
 	
 	
 }
